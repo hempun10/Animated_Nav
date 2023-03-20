81e80d78bd610dc0e18e44b8f0d4d932ce290447
Hem Pun <hemthegentlmen@gmail.com>
2023-03-20 09:03:37 +0545

    Code written in DRY

--- app.js
+++ app.js
@@ -1,32 +1,43 @@
-const menuBars = document.getElementById('menu-bars'),
-      overlay = document.getElementById('overlay'),
-      nav1 = document.getElementById('nav-1'),
-      nav2 = document.getElementById('nav-2'),
-      nav3 = document.getElementById('nav-3'),
-      nav4 = document.getElementById('nav-4'),
-      nav5 = document.getElementById('nav-5');
+const menuBars = document.getElementById("menu-bars"),
+  overlay = document.getElementById("overlay"),
+  nav1 = document.getElementById("nav-1"),
+  nav2 = document.getElementById("nav-2"),
+  nav3 = document.getElementById("nav-3"),
+  nav4 = document.getElementById("nav-4"),
+  nav5 = document.getElementById("nav-5"),
+  navItems = [nav1,nav2,nav3,nav4,nav5];
 
 
+// Control Navigation ANimation
 
-
-// Toggle Function 
-const toogleNav =() =>{
-    // toggle: Menu Bars Open/Closed
-    menuBars.classList.toggle('change')
-    // Toggle: Menu Active
-    overlay.classList.toggle('overlay-active')
-    if(overlay.classList.contains('overlay-active')){
-        // Animate In - Overlay
-        overlay.classList.add('overlay-slide-right')
-    }else{
-        overlay.classList.add('overlay-slide-left')
-    }
+const navAnimation =(direction1,direction2) =>{
+    navItems.forEach((nav ,i) =>{
+        nav.classList.replace(`slide-${direction1}-${i + 1}`,`slide-${direction2}-${i + 1}`)
+    })
 }
 
+// Toggle Function
+const toogleNav = () => {
+  // toggle: Menu Bars Open/Closed
+  menuBars.classList.toggle("change");
+  // Toggle: Menu Active
+  overlay.classList.toggle("overlay-active");
+  if (overlay.classList.contains("overlay-active")) {
+    // Animate In - Overlay
+    overlay.classList.replace('overlay-slide-left','overlay-slide-right')
+
+    // Animate IN- Nava Items
+    navAnimation('out','in')
+  } else {
+    overlay.classList.replace('overlay-slide-right','overlay-slide-left')
+
+    // Animate Out- Nava Items
+    navAnimation('in','out')
+  }
+};
+
 // Event Lsitners
-menuBars.addEventListener('click', toogleNav)
-nav1.addEventListener('click',toogleNav)
-nav2.addEventListener('click',toogleNav)
-nav3.addEventListener('click',toogleNav)
-nav4.addEventListener('click',toogleNav)
-nav5.addEventListener('click',toogleNav)
+menuBars.addEventListener("click", toogleNav);
+navItems.forEach((nav)=>{
+    nav.addEventListener('click',toogleNav)
+})
